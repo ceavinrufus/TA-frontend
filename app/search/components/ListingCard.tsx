2ea127b18036cd9154a1cbd709fc3ab35ec4e7c7
ceavinrufus <18221162@std.stei.itb.ac.id>
2025-05-05 17:42:58 +0700

    style: carousel for listing card in search

--- app/search/components/ListingCard.tsx
+++ app/search/components/ListingCard.tsx
@@ -4,6 +4,12 @@ import { SearchListing } from "@/types/listing";
 import Image from "next/image";
 import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
+import Autoplay from "embla-carousel-autoplay";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
 
 const ListingCard = ({ listing }: { listing: SearchListing }) => {
   const searchParams = useSearchParams();
@@ -22,15 +28,28 @@ const ListingCard = ({ listing }: { listing: SearchListing }) => {
       }}
       className="border items-start rounded-lg p-4 shadow hover:shadow-neumorphic-card-up transition-shadow duration-200 ease-in-out"
     >
-      {listing.pictures && listing.pictures[0] && (
-        <div className="relative w-full h-48 mb-2 rounded-lg overflow-hidden">
-          <Image
-            src={listing.pictures[0]}
-            alt={listing.name}
-            className="absolute object-cover"
-            fill
-          />
-        </div>
+      {listing.pictures && listing.pictures.length > 0 && (
+        <Carousel
+          plugins={[
+            Autoplay({
+              delay: 2000,
+            }),
+          ]}
+          className="w-full mb-2 rounded-lg overflow-hidden"
+        >
+          <CarouselContent>
+            {listing.pictures.map((picture, index) => (
+              <CarouselItem key={index} className="relative w-full h-48">
+                <Image
+                  src={picture}
+                  alt={`${listing.name} - Image ${index + 1}`}
+                  className="absolute object-cover"
+                  fill
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
       )}
       <h2 className="text-lg font-semibold">{listing.name}</h2>
       <p className="text-gray-600">{listing.address}</p>
