f611887bc302c72ca0b4331b9cb4540370079ae7
ceavinrufus <18221162@std.stei.itb.ac.id>
2025-05-07 00:25:48 +0700

    feat: add navbar again

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { cookieToInitialState } from "wagmi";
 import "./globals.css";
 import { getConfig } from "@/wagmi.config";
 import { Providers } from "./providers";
-// import { Navbar } from "@/components/Navbar";
+import { Navbar } from "@/components/Navbar";
 import { Toaster } from "@/components/ui/toaster";
 
 const poppins = Poppins({
@@ -36,7 +36,7 @@ export default async function RootLayout({
         <div className="fixed inset-0 w-full h-full bg-repeat bg-noise opacity-25 bg-[length:350px] z-[-20] before:content-[''] before:absolute before:w-[2500px] before:h-[2500px] before:rounded-full before:blur-[100px] before:-left-[1000px] before:-top-[2000px] before:bg-white before:opacity-50 before:z-[-100]"></div>
         <main className="flex flex-col max-w-screen-lg mx-auto pb-20">
           <Providers initialState={initialState}>
-            {/* <Navbar /> */}
+            <Navbar />
             {children}
             <Toaster />
           </Providers>
