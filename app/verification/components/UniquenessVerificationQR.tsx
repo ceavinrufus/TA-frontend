a12e929019aa682819879003ce3ab20c793815d2
ceavinrufus <18221162@std.stei.itb.ac.id>
2025-05-10 17:10:25 +0700

    feat: not unique validation

--- app/verification/components/UniquenessVerificationQR.tsx
+++ app/verification/components/UniquenessVerificationQR.tsx
@@ -20,61 +20,61 @@ const UniquenessVerificationQR = ({
   const { toast } = useToast();
   const { user, setUser } = useUserStore();
 
-  useEffect(() => {
-    const generateQR = async () => {
-      try {
-        const user = await getUserInfo();
+  const generateQR = async () => {
+    try {
+      const user = await getUserInfo();
 
-        if (!user) {
-          toast({
-            title: "Please connect your wallet",
-            description: "You need to connect your wallet to proceed.",
-            variant: "destructive",
-          });
+      if (!user) {
+        toast({
+          title: "Please connect your wallet",
+          description: "You need to connect your wallet to proceed.",
+          variant: "destructive",
+        });
+        return;
+      }
+
+      const randomNumber = Math.floor(Math.random() * 1000000);
+      // Include reservationId in the sessionId to make it reservation-specific
+      setSessionId(`${user.id}${randomNumber}`);
+
+      const {
+        data: { request },
+      } = await requestProof(
+        `${user.id}${randomNumber}`,
+        "Uniqueness verification",
+        {
+          allowedIssuers: ["*"],
+          context:
+            "https://raw.githubusercontent.com/anima-protocol/claims-polygonid/main/schemas/json-ld/pou-v1.json-ld",
+          credentialSubject: JSON.stringify({
+            unique: { $eq: true },
+          }),
+          proofType: "BJJSignature2021",
+          type: "AnimaProofOfUniqueness",
+        }
+      );
+
+      // Base64 encode the verification request
+      const base64EncodedRequest = btoa(JSON.stringify(request));
+
+      // Configure the Wallet URL (universal link)
+      const universalLink = `https://wallet.privado.id/#i_m=${base64EncodedRequest}`;
+
+      // Generate QR code
+      QRCode.toDataURL(universalLink, { width: 500 }, (err, url) => {
+        if (err) {
+          console.error("QR code generation failed:", err);
           return;
         }
+        setQrCode(url);
+        setIsPolling(true);
+      });
+    } catch (error) {
+      console.error("Proof request failed:", error);
+    }
+  };
 
-        const randomNumber = Math.floor(Math.random() * 1000000);
-        // Include reservationId in the sessionId to make it reservation-specific
-        setSessionId(`${user.id}${randomNumber}`);
-
-        const {
-          data: { request },
-        } = await requestProof(
-          `${user.id}${randomNumber}`,
-          "Uniqueness verification",
-          {
-            allowedIssuers: ["*"],
-            context:
-              "https://raw.githubusercontent.com/anima-protocol/claims-polygonid/main/schemas/json-ld/pou-v1.json-ld",
-            credentialSubject: JSON.stringify({
-              unique: { $eq: true },
-            }),
-            proofType: "BJJSignature2021",
-            type: "AnimaProofOfUniqueness",
-          }
-        );
-
-        // Base64 encode the verification request
-        const base64EncodedRequest = btoa(JSON.stringify(request));
-
-        // Configure the Wallet URL (universal link)
-        const universalLink = `https://wallet.privado.id/#i_m=${base64EncodedRequest}`;
-
-        // Generate QR code
-        QRCode.toDataURL(universalLink, { width: 500 }, (err, url) => {
-          if (err) {
-            console.error("QR code generation failed:", err);
-            return;
-          }
-          setQrCode(url);
-          setIsPolling(true);
-        });
-      } catch (error) {
-        console.error("Proof request failed:", error);
-      }
-    };
-
+  useEffect(() => {
     generateQR();
   }, []);
 
@@ -84,7 +84,7 @@ const UniquenessVerificationQR = ({
     const poll = setInterval(async () => {
       try {
         const response = await getVerificationResult(sessionId);
-        console.log("Polling:", sessionId, response);
+
         if (response && response.from) {
           clearInterval(poll);
           setIsPolling(false);
@@ -95,8 +95,31 @@ const UniquenessVerificationQR = ({
             is_uniqueness_verified: true,
             did: response.from,
           };
+          try {
+            await updateUser(user.id, newUser);
+          } catch (error) {
+            const errorMessage = (error as Error).message;
+            console.error("Error updating user:", errorMessage);
+            if (errorMessage === "UQ_user_did") {
+              toast({
+                title: "Verification failed",
+                description:
+                  "You are not unique! Please use your existing account.",
+                variant: "destructive",
+              });
+            } else {
+              toast({
+                title: "Error updating user",
+                description: "Failed to update user information.",
+                variant: "destructive",
+              });
+            }
+            setIsPolling(true);
+            setQrCode(undefined);
+            generateQR();
+            return;
+          }
           setUser(newUser);
-          await updateUser(user.id, newUser);
 
           onScanSuccess(); // callback
         }
